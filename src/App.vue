c3f878de698d2aee478caaab57414e66c2cbf21f
Frank <frank@admindeMacBook-Pro.local>
2022-11-18 18:06:21 +0800

    feat(dependencies): vue-i18n

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <script setup lang="ts">
+import { ElConfigProvider } from 'element-plus'
+import { useLanguage } from '@/i18n'
+
+const { elementLocale } = useLanguage()
+
 </script>
 
 <template lang="pug">
-router-view
+el-config-provider(:locale="elementLocale")
+  router-view
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import Router from './router'
 import Pinia from './plugin/pinia'
+import I18n from './i18n/install'
 
 const app = createApp(App)
 app.use(Router)
 app.use(Pinia)
+app.use(I18n)
 app.mount('#app')
